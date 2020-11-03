8d7ecd71e4ffb3772b4d7405806a1a1d57ed094b
sandrolivaja <sandro.livaja@edu.fit.ba>
2020-11-03 10:47:26 +0300

    added a new project, still missing reports and authorization

---
+++ Mobitel.CustomerFacing/Mobitel.CustomerFacing/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Mobitel.CustomerFacing.ViewModels;
+
+namespace Mobitel.CustomerFacing.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Mobitel.CustomerFacing/Mobitel.CustomerFacing/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Mobitel.CustomerFacing.Models;
+using Mobitel.CustomerFacing.ViewModels;
+
+namespace Mobitel.CustomerFacing.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
